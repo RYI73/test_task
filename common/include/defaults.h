c500a6701cd576b618a3c9df077ed096dad21f72
Ruslan <rvalovyi@gmail.com>
2025-12-19 10:04:00 +0200

    'server' has been tested on RP Zero

--- common/include/defaults.h
+++ common/include/defaults.h
@@ -14,23 +14,29 @@
 /***********************************************************************************************/
 #define SOCKET_READ_TIMEOUT_MS                  (500)
 /***********************************************************************************************/
+/** Localhost IPv4 address (string form) */
+#define LOCALHOST_ADDR                          "127.0.0.1"
+/** INADDR_ANY IPv4 address (string form) */
+#define ANY_ADDR                                "0.0.0.0"
+/** TUN IPv4 address (string form) */
+#define TUN_ADDR                                "10.0.0.1"
 /** Server IPv4 address (string form) */
-#define SERVER_ADDR "127.0.0.1"
+#define SERVER_ADDR                             "192.168.1.122"
 /** Server TCP port */
-#define SERVER_PORT 12345
+#define SERVER_PORT                             12345
 /** Message sent to server */
-#define CLIENT_MESSAGE "HELLO"
+#define CLIENT_MESSAGE                          "HELLO"
 /** Maximum size of packet */
-#define PACKET_SIZE (1024)
+#define PACKET_SIZE                             (1024)
 /** Maximum number of concurrent clients */
-#define MAX_CLIENTS      5
+#define MAX_CLIENTS                             5
 /** poll() timeout in milliseconds */
-#define POLL_TIMEOUT_MS  500
+#define POLL_TIMEOUT_MS                         500
 /***********************************************************************************************/
 /** Expected message from client */
-#define EXPECTED_STRING  "HELLO"
+#define EXPECTED_STRING                         "HELLO"
 /** Reply sent on successful match */
-#define OK_REPLY         "OK"
+#define OK_REPLY                                "OK"
 /** Reply sent on mismatch */
-#define ERR_REPLY        "ERROR"
+#define ERR_REPLY                               "ERROR"
 /***********************************************************************************************/
